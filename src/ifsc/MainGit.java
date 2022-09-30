c9a7dbaf22d3e349f59ccb077442064b09c11e64
joaomiguelmarchi <jmsmarchi@gmail.com>
2022-09-30 12:10:05 -0300

    Pratica git 30/09

--- src/ifsc/MainGit.java
+++ src/ifsc/MainGit.java
@@ -1,5 +1,30 @@
 package ifsc;
 
+/**
+ * 
+ * @author João Miguel
+ *
+ */
+
 public class MainGit {
 
+	public static void main(String[] args) {
+	
+	}
+	/**
+	 * 
+	 * Pega um vetor e multiplica todos os elementos
+	 * desse vetor e retorna um novo vetor.
+	 * 
+	 * @param vetor
+	 * @return a
+	 */
+	
+	public Double[] multiplicaVetor(Double[] vetor) {
+		Double[] a = new Double[vetor.length];
+		for (int i=0;i<a.length; i++) {
+			a[i] = vetor[i] * vetor[i];
+		}
+		return a;
+	}
 }
